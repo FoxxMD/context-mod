fc20ee95613f3f5d5de5e13c25d8ada30cc5574a
FoxxMD <FoxxMD@users.noreply.github.com>
2021-06-04 10:17:18 -0400

    Truncate report content length to 100 characters to fit reddit spec

--- src/Action/ReportAction.ts
+++ src/Action/ReportAction.ts
@@ -1,6 +1,11 @@
 import {ActionJSONConfig, ActionConfig, ActionOptions} from "./index";
 import Action from "./index";
 import Snoowrap, {Comment, Submission} from "snoowrap";
+import {truncateStringToLength} from "../util";
+
+// https://www.reddit.com/dev/api/oauth#POST_api_report
+// denotes 100 characters maximum
+const reportTrunc = truncateStringToLength(100);
 
 export class ReportAction extends Action {
     content: string;
@@ -8,18 +13,18 @@ export class ReportAction extends Action {
 
     constructor(options: ReportActionOptions) {
         super(options);
-        this.content = options.content;
+        this.content = options.content || '';
     }
 
     async handle(item: Comment | Submission, client: Snoowrap): Promise<void> {
         // @ts-ignore
-        await item.report({reason: this.content});
+        await item.report({reason: reportTrunc(this.content)});
     }
 }
 
 export interface ReportActionConfig {
     /**
-     * The text of the report
+     * The text of the report. If longer than 100 characters will be truncated to "[content]..."
      * */
     content: string,
 }
