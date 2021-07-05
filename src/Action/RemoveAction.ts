4c3f9ee0825e0aa79a4772e6c56137a33692594a
FoxxMD <FoxxMD@users.noreply.github.com>
2021-07-05 15:37:37 -0400

    Fix remove check on remove action

--- src/Action/RemoveAction.ts
+++ src/Action/RemoveAction.ts
@@ -2,6 +2,7 @@ import {ActionJson, ActionConfig} from "./index";
 import Action from "./index";
 import Snoowrap, {Comment, Submission} from "snoowrap";
 import {RuleResult} from "../Rule";
+import {activityIsRemoved} from "../Utils/SnoowrapUtils";
 
 export class RemoveAction extends Action {
     getKind() {
@@ -11,7 +12,7 @@ export class RemoveAction extends Action {
     async process(item: Comment | Submission, ruleResults: RuleResult[]): Promise<void> {
         // issue with snoowrap typings, doesn't think prop exists on Submission
         // @ts-ignore
-        if (item.removed === true) {
+        if (activityIsRemoved(item)) {
             this.logger.warn('Item is already removed');
             return;
         }
