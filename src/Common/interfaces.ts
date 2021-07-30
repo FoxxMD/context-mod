bf336ca55a444dabb463669ec29b744612e14f49
FoxxMD <FoxxMD@users.noreply.github.com>
2021-07-30 12:00:42 -0400

    Move the schema/settings for operator into json schema (viewer)
    easier to read, has full examples, and validation

--- src/Common/interfaces.ts
+++ src/Common/interfaces.ts
@@ -649,6 +649,9 @@ export interface RegExResult {
 }
 
 type LogLevel = "error" | "warn" | "info" | "verbose" | "debug";
+/**
+ * Available cache providers
+ * */
 export type CacheProvider = 'memory' | 'redis' | 'none';
 
 // export type StrongCache = SubredditCacheConfig & {
@@ -661,13 +664,57 @@ export type StrongCache = {
     provider: CacheOptions
 }
 
+/**
+ * Configure granular settings for a cache provider with this object
+ * */
 export interface CacheOptions {
     store: CacheProvider,
+    /**
+     * (`redis`) hostname
+     *
+     * @default "localhost"
+     * @examples ["localhost"]
+     * */
     host?: string | undefined,
+    /**
+     * (`redis`) port to connect on
+     *
+     * @default 6379
+     * @examples [6379]
+     * */
     port?: number | undefined,
+    /**
+     * (`redis``) the authentication passphrase (if enabled)
+     * */
     auth_pass?: string | undefined,
+    /**
+     * (`redis`) the db number to use
+     *
+     * @default 0
+     * @examples [0]
+     * */
     db?: number | undefined,
+    /**
+     * The default TTL, in seconds, for the cache provider.
+     *
+     * Can mostly be ignored since TTLs are defined for each cache object
+     *
+     * @default 60
+     * @examples [60]
+     * */
     ttl?: number,
+    /**
+     * (`memory`) The maximum number of keys to store in cache
+     *
+     * This will determine roughly how large in **RAM** each `memory` cache can be, based on how large your `window` criteria are. Consider this example:
+     *
+     * * `"window": 100` (in your rules)
+     * * `"max: 500"`
+     * * Maximum size of cache will be `500 x 100 activities = 50,000 activities`
+     *
+     * @default 500
+     * @examples [500]
+     * */
     max?: number
 }
 
@@ -701,6 +748,9 @@ export interface NotificationContent {
 export type NotificationEvents = (NotificationEventType[] | NotificationEventConfig)[];
 
 export interface NotificationConfig {
+    /**
+     * A list of notification providers (Discord, etc..) to configure. Each object in the list is one provider. Multiple of the same provider can be provided but must have different names
+     * */
     providers: NotificationProviders[],
     events: NotificationEvents
 }
@@ -716,7 +766,16 @@ export interface ManagerStateChangeOption {
     suppressNotification?: boolean
 }
 
+/**
+ * Configuration for application-level settings IE for running the bot instance
+ *
+ * * To load a JSON configuration **from the command line** use the `-c` cli argument EX: `node src/index.js -c /path/to/JSON/config.json`
+ * * To load a JSON configuration **using an environmental variable** use `OPERATOR_CONFIG` EX: `OPERATOR_CONFIG=/path/to/JSON/config.json`
+ * */
 export interface OperatorJsonConfig {
+    /**
+    * Settings related to the user(s) running this ContextMod instance and information on the bot
+    * */
     operator?: {
         /**
          * The name of the Reddit account, without prefix, that the operator of this bot uses.
@@ -724,86 +783,347 @@ export interface OperatorJsonConfig {
          * This is used for showing more information in the web interface IE show all logs/subreddits if even not a moderator.
          *
          * EX -- User is /u/FoxxMD then `"name": "FoxxMD"`
+         *
+         * * ENV => `OPERATOR`
+         * * ARG => `--operator <name>`
+         *
+         * @examples ["FoxxMD"]
          * */
         name?: string,
         /**
          * A **public** name to display to users of the web interface. Use this to help moderators using your bot identify who is the operator in case they need to contact you.
          *
          * Leave undefined for no public name to be displayed.
+         *
+         * * ENV => `OPERATOR_DISPLAY`
+         * * ARG => `--operatorDisplay <name>`
+         *
+         * @examples ["Moderators of r/MySubreddit"]
          * */
         display?: string,
         /**
          * The name to use when identifying the bot. Defaults to name of the authenticated Reddit account IE `u/yourBotAccount`
+         *
+         * @examples ["u/yourBotAccount"]
          * */
         botName?: string,
     },
+    /**
+    * The credentials required for the bot to interact with Reddit's API
+    *
+    * **Note:** Only `clientId` and `clientSecret` are required for initial setup (to use the oauth helper) **but ALL are required to properly run the bot.**
+    * */
     credentials?: {
+        /**
+         * Client ID for your Reddit application
+         *
+         * * ENV => `CLIENT_ID`
+         * * ARG => `--clientId <id>`
+         *
+         * @examples ["f4b4df1c7b2"]
+         * */
         clientId?: string,
+        /**
+         * Client Secret for your Reddit application
+         *
+         * * ENV => `CLIENT_SECRET`
+         * * ARG => `--clientSecret <id>`
+         *
+         * @examples ["34v5q1c56ub"]
+         * */
         clientSecret?: string,
+        /**
+        * Redirect URI for your Reddit application
+        *
+        * Only required if running ContextMod with a web interface (and after using oauth helper)
+        *
+        * * ENV => `REDIRECT_URI`
+        * * ARG => `--redirectUri <uri>`
+        *
+        * @examples ["http://localhost:8085"]
+        * @format uri
+        * */
         redirectUri?: string,
+        /**
+         * Access token retrieved from authenticating an account with your Reddit Application
+         *
+         * * ENV => `ACCESS_TOKEN`
+         * * ARG => `--accessToken <token>`
+         *
+         * @examples ["p75_1c467b2"]
+         * */
         accessToken?: string,
+        /**
+         * Refresh token retrieved from authenticating an account with your Reddit Application
+         *
+         * * ENV => `REFRESH_TOKEN`
+         * * ARG => `--refreshToken <token>`
+         *
+         * @examples ["34_f1w1v4"]
+         * */
         refreshToken?: string
     },
+    /**
+     * Settings to configure 3rd party notifications for when ContextMod behavior occurs
+     * */
     notifications?: NotificationConfig
+    /**
+     * Settings to configure global logging defaults
+     * */
     logging?: {
+        /**
+         * The minimum log level to output. The log level set will output logs at its level **and all levels above it:**
+         *
+         *  * `error`
+         *  * `warn`
+         *  * `info`
+         *  * `verbose`
+         *  * `debug`
+         *
+         *  Note: `verbose` will display *a lot* of information on the status/result of run rules/checks/actions etc. which is very useful for testing configurations. Once your bot is stable changing the level to `info` will reduce log noise.
+         *
+         *  * ENV => `LOG_LEVEL`
+         *  * ARG => `--logLevel <level>`
+         *
+         *  @default "verbose"
+         *  @examples ["verbose"]
+         * */
         level?: LogLevel,
+        /**
+         * The absolute path to a directory where rotating log files should be stored.
+         *
+         * * If not present or `null` no log files will be created
+         * * If `true` logs will be stored at `[working directory]/logs`
+         *
+         * * ENV => `LOG_DIR`
+         * * ARG => `--logDir [dir]`
+         *
+         * @examples ["/var/log/contextmod"]
+         * */
         path?: string,
     },
+    /**
+     * Settings to control some [Snoowrap](https://github.com/not-an-aardvark/snoowrap) behavior
+     * */
     snoowrap?: {
+        /**
+         * Proxy all requests to Reddit's API through this endpoint
+         *
+         * * ENV => `PROXY`
+         * * ARG => `--proxy <proxyEndpoint>`
+         *
+         * @examples ["http://localhost:4443"]
+         * */
         proxy?: string,
+        /**
+         * Manually set the debug status for snoowrap
+         *
+         * When snoowrap has `debug: true` it will log the http status response of reddit api requests to at the `debug` level
+         *
+         * * Set to `true` to always output
+         * * Set to `false` to never output
+         *
+         * If not present or `null` will be set based on `logLevel`
+         *
+         * * ENV => `SNOO_DEBUG`
+         * * ARG => `--snooDebug`
+         * */
         debug?: boolean,
     }
+    /**
+     * Settings related to bot behavior for subreddits it is managing
+     * */
     subreddits?: {
+        /**
+         * Names of subreddits for bot to run on
+         *
+         * If not present or `null` bot will run on all subreddits it is a moderator of
+         *
+         * * ENV => `SUBREDDITS` (comma-separated)
+         * * ARG => `--subreddits <list...>`
+         *
+         * @examples [["mealtimevideos","programminghumor"]]
+         * */
         names?: string[],
+        /**
+         * If `true` then all subreddits will run in dry run mode, overriding configurations
+         *
+         * * ENV => `DRYRUN`
+         * * ARG => `--dryRun`
+         *
+         * @default false
+         * @examples [false]
+         * */
         dryRun?: boolean,
+        /**
+         * The default relative url to the ContextMod wiki page EX `https://reddit.com/r/subreddit/wiki/<path>`
+         *
+         * * ENV => `WIKI_CONFIG`
+         * * ARG => `--wikiConfig <path>`
+         *
+         * @default "botconfig/contextbot"
+         * @examples ["botconfig/contextbot"]
+         * */
         wikiConfig?: string,
+        /**
+         * Interval, in seconds, to perform application heartbeat
+         *
+         * On heartbeat the application does several things:
+         *
+         * * Log output with current api rate remaining and other statistics
+         * * Tries to retrieve and parse configurations for any subreddits with invalid configuration state
+         * * Restarts any bots stopped/paused due to polling issues, general errors, or invalid configs (if new config is valid)
+         *
+         * * ENV => `HEARTBEAT`
+         * * ARG => `--heartbeat <sec>`
+         *
+         * @default 300
+         * @examples [300]
+         * */
         heartbeatInterval?: number,
     },
+    /**
+     *  Settings related to default polling configurations for subreddits
+     * */
     polling?: PollingDefaults & {
+        /**
+         * If set to `true` all subreddits polling unmoderated/modqueue with default polling settings will share a request to "r/mod"
+         * otherwise each subreddit will poll its own mod view
+         *
+         * * ENV => `SHARE_MOD`
+         * * ARG => `--shareMod`
+         *
+         * @default false
+         * */
         sharedMod?: boolean,
-        limit?: number,
-        interval?: number,
     },
+    /**
+     * Settings related to default configurations for queue behavior for subreddits
+     * */
     queue?: {
+        /**
+         * Set the number of maximum concurrent workers any subreddit can use.
+         *
+         * Subreddits may define their own number of max workers in their config but the application will never allow any subreddit's max workers to be larger than the operator
+         *
+         * NOTE: Do not increase this unless you are certain you know what you are doing! The default is suitable for the majority of use cases.
+         *
+         * @default 1
+         * @examples [1]
+         * */
       maxWorkers?: number,
     },
+    /**
+     * Settings for the web interface
+     * */
     web?: {
+        /**
+         * Whether the web server interface should be started
+         *
+         * In most cases this does not need to be specified as the application will automatically detect if it is possible to start it --
+         * use this to specify "cli only" behavior if you encounter errors with port/address or are paranoid
+         *
+         * * ENV => `WEB`
+         * * ARG => `node src/index.js run [interface]` -- interface can be `web` or `cli`
+         *
+         * @default true
+         * */
         enabled?: boolean,
+        /**
+         * The port for the web interface
+         *
+         * * ENV => `PORT`
+         * * ARG => `--port <number>`
+         *
+         * @default 8085
+         * @examples [8085]
+         * */
         port?: number,
+        /**
+         * Settings to configure the behavior of user sessions -- the session is what the web interface uses to identify logged in users.
+         * */
         session?: {
+            /**
+             * The cache provider to use.
+             *
+             * The default should be sufficient for almost all use cases
+             *
+             * @default "memory"
+             * @examples ["memory"]
+             * */
             provider?: 'memory' | 'redis' | CacheOptions,
+            /**
+             * The secret value used to encrypt session data
+             *
+             * If provider is persistent (redis) specifying a value here will ensure sessions are valid between application restarts
+             *
+             * When not present or `null` a random string is generated on application start
+             *
+             * @examples ["definitelyARandomString"]
+             * */
             secret?: string,
         }
+        /**
+         * The default log level to filter to in the web interface
+         *
+         * If not specified or `null` will be same as global `logLevel`
+         * */
         logLevel?: LogLevel,
+        /**
+         * Maximum number of log statements to keep in memory for each subreddit
+         *
+         * @default 200
+         * @examples [200]
+         * */
         maxLogs?: number,
     }
-    // caching?:  (SubredditCacheConfig & {
-    //     provider?: CacheProvider | CacheOptions | undefined
-    // }) | CacheProvider | undefined
+    /**
+     * Settings to configure the default caching behavior for each suberddit
+     * */
     caching?: {
         /**
-         * Amount of time, in milliseconds, author activities (Comments/Submission) should be cached
-         * @examples [10000]
-         * @default 10000
+         * Amount of time, in seconds, author activity history (Comments/Submission) should be cached
+         *
+         * * ENV => `AUTHOR_TTL`
+         * * ARG => `--authorTTL <sec>`
+         * @examples [60]
+         * @default 60
          * */
         authorTTL?: number;
         /**
-         * Amount of time, in milliseconds, wiki content pages should be cached
-         * @examples [300000]
-         * @default 300000
+         * Amount of time, in seconds, wiki content pages should be cached
+         * @examples [300]
+         * @default 300
          * */
         wikiTTL?: number;
-
         /**
-         * Amount of time, in milliseconds, [Toolbox User Notes](https://www.reddit.com/r/toolbox/wiki/docs/usernotes) should be cached
-         * @examples [60000]
-         * @default 60000
+         * Amount of time, in seconds, [Toolbox User Notes](https://www.reddit.com/r/toolbox/wiki/docs/usernotes) should be cached
+         * @examples [300]
+         * @default 300
          * */
         userNotesTTL?: number;
+        /**
+         * The cache provider and, optionally, a custom configuration for that provider
+         * */
         provider?: CacheProvider | CacheOptions
     } | CacheProvider
+    /**
+     * Settings related to managing heavy API usage.
+     * */
     api?: {
+        /**
+         * When `api limit remaining` reaches this number the application will attempt to put heavy-usage subreddits in a **slow mode** where activity processed is slowed to one every 1.5 seconds until the api limit is reset.
+         *
+         * @default 250
+         * @examples [250]
+         * */
         softLimit?: number,
+        /**
+         * When `api limit remaining` reaches this number the application will pause all event polling until the api limit is reset.
+         *
+         * @default 50
+         * @examples [50]
+         * */
         hardLimit?: number,
     }
 }
