c9d8bf637b57022eeb26445977bc87bf43843237
FoxxMD <FoxxMD@users.noreply.github.com>
2022-09-26 16:59:46 -0400

    chore: Add tests for item criteria

--- tests/snoowrapUtils.test.ts
+++ tests/snoowrapUtils.test.ts
@@ -4,66 +4,42 @@ import {mock, spy, when, instance} from 'ts-mockito';
 import Snoowrap from "snoowrap";
 import {Submission, Comment} from "snoowrap/dist/objects";
 import {activityIsDeleted, activityIsFiltered, activityIsRemoved} from "../src/Utils/SnoowrapUtils";
-
-const mockSnoowrap = new Snoowrap({userAgent: 'test', accessToken: 'test'});
+import {sampleActivity} from "./testFactory";
 
 describe('Activity state recognition', function () {
     describe('activity is removed', function () {
         describe('when bot is a moderator', function () {
             it('submission not removed when filtered by automod', function () {
 
-                assert.isFalse(activityIsRemoved(new Submission({
-                    can_mod_post: true,
-                    banned_at_utc: 12345,
-                    removed_by_category: 'automod_filtered'
-                }, mockSnoowrap, true)));
+                assert.isFalse(activityIsRemoved(sampleActivity.moddable.activityFilteredByAutomod()));
 
             })
             it('submission is removed when not filtered by automod', function () {
 
-                assert.isTrue(activityIsRemoved(new Submission({
-                    can_mod_post: true,
-                    banned_at_utc: 12345,
-                    removed_by_category: 'mod'
-                }, mockSnoowrap, true)));
+                assert.isTrue(activityIsRemoved(sampleActivity.moddable.activityRemovedByMod()));
 
             })
             it('comment is removed', function () {
 
-                assert.isTrue(activityIsRemoved(new Comment({
-                    can_mod_post: true,
-                    banned_at_utc: 12345,
-                    removed: true,
-                    replies: ''
-                }, mockSnoowrap, true)));
+                assert.isTrue(activityIsRemoved(sampleActivity.moddable.commentRemoved()));
+                assert.isTrue(activityIsRemoved(sampleActivity.moddable.commentRemovedByMod()));
 
             })
         })
         describe('when bot is not a moderator', function () {
             it('submission is deleted by moderator', function () {
 
-                assert.isTrue(activityIsRemoved(new Submission({
-                    can_mod_post: false,
-                    removed_by_category: 'moderator'
-                }, mockSnoowrap, true)));
+                assert.isTrue(activityIsRemoved(sampleActivity.public.submissionRemoved()));
 
             })
             it('submission is deleted by user or other', function () {
 
-                assert.isTrue(activityIsRemoved(new Submission({
-                    can_mod_post: false,
-                    removed_by_category: 'deleted'
-                }, mockSnoowrap, true)));
-
+                assert.isTrue(activityIsRemoved(sampleActivity.moddable.submissionDeleted()));
             })
 
             it('comment body is removed', function () {
 
-                assert.isTrue(activityIsRemoved(new Comment({
-                    can_mod_post: false,
-                    body: '[removed]',
-                    replies: ''
-                }, mockSnoowrap, true)));
+                assert.isTrue(activityIsRemoved(sampleActivity.public.commentRemoved()));
 
             })
         })
@@ -71,31 +47,17 @@ describe('Activity state recognition', function () {
 
     describe('activity is filtered', function() {
         it('not filtered when user is not a moderator', function() {
-            assert.isFalse(activityIsFiltered(new Submission({
-                can_mod_post: false,
-                banned_at_utc: 12345,
-                removed_by_category: 'mod'
-            }, mockSnoowrap, true)));
+            assert.isFalse(activityIsFiltered(sampleActivity.public.activityRemoved()));
         })
 
         it('submission is filtered', function () {
 
-            assert.isTrue(activityIsFiltered(new Submission({
-                can_mod_post: true,
-                banned_at_utc: 12345,
-                removed_by_category: 'automod_filtered'
-            }, mockSnoowrap, true)));
-
+            assert.isTrue(activityIsFiltered(sampleActivity.moddable.activityFilteredByAutomod()));
         })
 
         it('comment is filtered', function () {
 
-            assert.isTrue(activityIsFiltered(new Comment({
-                can_mod_post: true,
-                banned_at_utc: 12345,
-                removed: false,
-                replies: ''
-            }, mockSnoowrap, true)));
+            assert.isTrue(activityIsFiltered(sampleActivity.moddable.commentFiltered()));
 
         })
     })
@@ -104,25 +66,14 @@ describe('Activity state recognition', function () {
 
         it('submission is deleted', function () {
 
-            assert.isTrue(activityIsDeleted(new Submission({
-                can_mod_post: true,
-                banned_at_utc: 12345,
-                removed_by_category: 'deleted'
-            }, mockSnoowrap, true)));
+            assert.isTrue(activityIsDeleted(sampleActivity.moddable.submissionDeleted()));
+            assert.isTrue(activityIsDeleted(sampleActivity.public.submissionDeleted()));
 
         })
 
         it('comment is deleted', function () {
 
-            assert.isTrue(activityIsDeleted(new Comment({
-                can_mod_post: true,
-                banned_at_utc: 12345,
-                removed: false,
-                replies: '',
-                author: {
-                    name: '[deleted]'
-                }
-            }, mockSnoowrap, true)));
+            assert.isTrue(activityIsDeleted(sampleActivity.moddable.commentDeleted()));
 
         })
 
---
+++ tests/testFactory.ts
@@ -0,0 +1,196 @@
+import {OperatorConfig, OperatorJsonConfig} from "../src/Common/interfaces";
+import Snoowrap from "snoowrap";
+import Bot from "../src/Bot/index"
+import {buildOperatorConfigWithDefaults} from "../src/ConfigBuilder";
+import {App} from "../src/App";
+import {YamlOperatorConfigDocument} from "../src/Common/Config/Operator";
+import {NoopLogger} from "../src/Utils/loggerFactory";
+import {ManagerEntity} from "../src/Common/Entities/ManagerEntity";
+import {Bot as BotEntity} from "../src/Common/Entities/Bot";
+import {SubredditResources} from "../src/Subreddit/SubredditResources";
+import {Subreddit, Comment, Submission} from 'snoowrap/dist/objects';
+import dayjs from 'dayjs';
+
+const mockSnoowrap = new Snoowrap({userAgent: 'test', accessToken: 'test'});
+
+const memoryConfig: OperatorJsonConfig = {
+    databaseConfig: {
+        connection: {
+            type: 'sqljs',
+            location: ':memory:'
+        }
+    },
+    logging: {
+        level: 'debug',
+        file: {
+            dirname: false
+        }
+    },
+    bots: [
+        {
+            name: 'test',
+            credentials: {
+                reddit: {
+                    clientId: 'test',
+                    clientSecret: 'test',
+                    accessToken: 'test',
+                    refreshToken: 'test'
+                }
+            }
+        }
+    ]
+};
+
+let config: OperatorConfig;
+let app: App;
+let snoowrap: Snoowrap;
+let bot: Bot;
+let resource: SubredditResources;
+let subreddit: Subreddit;
+
+export const getConfig = async () => {
+    if (config === undefined) {
+        config = await buildOperatorConfigWithDefaults(memoryConfig);
+    }
+    return config;
+}
+
+export const getApp = async () => {
+    if (app === undefined) {
+        const config = await getConfig();
+        app = new App({...config, fileConfig: {document: new YamlOperatorConfigDocument('')}});
+        await app.initDatabase();
+    }
+    return app;
+}
+
+export const getSnoowrap = async () => {
+    if (snoowrap === undefined) {
+        const bot = await getBot();
+        snoowrap = bot.client;
+    }
+    return snoowrap;
+}
+
+export const getBot = async () => {
+    if (bot === undefined) {
+        await getApp();
+        const config = await getConfig();
+        bot = new Bot(config.bots[0], NoopLogger);
+        await bot.cacheManager.set('test', {
+            logger: NoopLogger,
+            subreddit: bot.client.getSubreddit('test'),
+            client: bot.client,
+            statFrequency: 'minute',
+            managerEntity: new ManagerEntity(),
+            botEntity: new BotEntity()
+        });
+    }
+    return bot;
+}
+
+export const getResource = async () => {
+    if (resource === undefined) {
+        const bot = await getBot();
+        resource = bot.cacheManager.get('test') as SubredditResources;
+    }
+    return resource;
+}
+
+// @ts-ignore
+export const getSubreddit = async () => {
+    if (subreddit === undefined) {
+        const snoo = await getSnoowrap();
+        subreddit = new Subreddit({id: 't3_test', name: 'test'}, snoo, true);
+    }
+    // @ts-ignore
+    return subreddit;
+}
+
+export const sampleActivity = {
+    moddable: {
+        commentRemovedByMod: (snoowrap = mockSnoowrap) => {
+            return new Submission({
+                can_mod_post: true,
+                banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+                removed_by_category: 'mod'
+            }, snoowrap, true);
+        },
+        activityRemovedByMod: (snoowrap = mockSnoowrap) => {
+            return new Submission({
+                can_mod_post: true,
+                banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+                removed_by_category: 'mod'
+            }, snoowrap, true)
+        },
+        activityFilteredByAutomod: (snoowrap = mockSnoowrap) => {
+            return new Submission({
+                can_mod_post: true,
+                banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+                removed_by_category: 'automod_filtered'
+            }, snoowrap, true);
+        },
+        commentFiltered: (snoowrap = mockSnoowrap) => {
+            return new Comment({
+                can_mod_post: true,
+                banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+                removed: false,
+                replies: ''
+            }, snoowrap, true)
+        },
+        commentRemoved: (snoowrap = mockSnoowrap) => {
+          return new Comment({
+              can_mod_post: true,
+              banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+              removed: true,
+              replies: ''
+          }, snoowrap, true);
+        },
+        submissionDeleted: (snoowrap = mockSnoowrap) => {
+            return new Submission({
+                can_mod_post: true,
+                banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+                removed_by_category: 'deleted'
+            }, snoowrap, true);
+        },
+        commentDeleted: (snoowrap = mockSnoowrap) => {
+            return new Comment({
+                can_mod_post: true,
+                banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+                removed: false,
+                replies: '',
+                author: {
+                    name: '[deleted]'
+                }
+            }, snoowrap, true);
+        }
+    },
+    public: {
+        submissionRemoved: (snoowrap = mockSnoowrap) => {
+            return new Submission({
+                can_mod_post: false,
+                removed_by_category: 'moderator'
+            }, snoowrap, true)
+        },
+        submissionDeleted: (snoowrap = mockSnoowrap) => {
+            return new Submission({
+                can_mod_post: false,
+                removed_by_category: 'deleted'
+            }, snoowrap, true);
+        },
+        commentRemoved: (snoowrap = mockSnoowrap) => {
+            return new Comment({
+                can_mod_post: false,
+                body: '[removed]',
+                replies: ''
+            }, snoowrap, true)
+        },
+        activityRemoved: (snoowrap = mockSnoowrap) => {
+            return new Submission({
+                can_mod_post: false,
+                banned_at_utc: dayjs().subtract(10, 'minutes').unix(),
+                removed_by_category: 'moderator'
+            }, snoowrap, true);
+        }
+    }
+}
