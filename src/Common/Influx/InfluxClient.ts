7fb69ae67aa691706edd5fef7f61210158c1a595
FoxxMD <FoxxMD@users.noreply.github.com>
2022-10-13 11:39:45 -0400

    fix: Attempt to decrease frequency of influxdb timeout errors
    * Use keep-alive http agent to reuse open connections
    * Decrease max batch size and flush interval to payload sent is smaller
    * Add debug logging for fail/success/retry events on flush

--- src/Common/Influx/InfluxClient.ts
+++ src/Common/Influx/InfluxClient.ts
@@ -1,10 +1,12 @@
 import {InfluxConfig} from "./interfaces";
-import {InfluxDB, Point, WriteApi, setLogger} from "@influxdata/influxdb-client";
+import {InfluxDB, Point, WriteApi, setLogger, DEFAULT_WriteOptions, ClientOptions, DEFAULT_RetryDelayStrategyOptions} from "@influxdata/influxdb-client";
 import {HealthAPI} from "@influxdata/influxdb-client-apis";
 import dayjs, {Dayjs} from "dayjs";
 import {Logger} from "winston";
 import {mergeArr} from "../../util";
 import {CMError} from "../../Utils/Errors";
+import {Agent} from 'http';
+import {WriteOptions} from "@influxdata/influxdb-client/dist";
 
 export interface InfluxClientConfig extends InfluxConfig {
     client?: InfluxDB
@@ -34,13 +36,14 @@ export class InfluxClient {
 
         this.config = rest;
         this.ready = ready;
-        if(client !== undefined) {
+        if (client !== undefined) {
             this.client = client;
         } else {
-           this.client = InfluxClient.createClient(this.config);
-           setLogger(this.logger);
+            this.client = InfluxClient.createClient(this.config);
+            setLogger(this.logger);
         }
-        this.write = this.client.getWriteApi(config.credentials.org, config.credentials.bucket, 'ms');
+
+        this.write = this.client.getWriteApi(config.credentials.org, config.credentials.bucket, 'ms', InfluxClient.createWriteOptions(this.config, this.logger));
         this.tags = tags;
         this.write.useDefaultTags(tags);
         this.health = new HealthAPI(this.client);
@@ -96,13 +99,70 @@ export class InfluxClient {
     }
 
     static createClient(config: InfluxConfig): InfluxDB {
-        return new InfluxDB({
-            url: config.credentials.url,
-            token: config.credentials.token,
+        const {
+            credentials,
+            useKeepAliveAgent = true,
+        } = config;
+
+        const clientOptions: ClientOptions = {
+            url: credentials.url,
+            token: credentials.token,
+            writeOptions: InfluxClient.createWriteOptions(config),
+        }
+        if (useKeepAliveAgent) {
+            // reusing connection
+            // https://github.com/influxdata/influxdb-client-js/issues/393#issuecomment-985272866
+            const agent = new Agent({
+                keepAlive: true,
+                keepAliveMsecs: 20 * 1000, // 20 seconds keep alive
+            })
+            process.on('exit', () => agent.destroy())
+            clientOptions.transportOptions = {agent};
+        }
+        return new InfluxDB(clientOptions);
+    }
+
+    static createWriteOptions(config: InfluxConfig, logger?: Logger): Partial<WriteOptions> {
+        const {
             writeOptions: {
-                defaultTags: config.defaultTags
+                defaultTags: userDefinedDefaultTags = {},
+                ...restUserWriteOptions
+            } = {
+                batchSize: 500,
+                maxRetries: 5,
+                // 30 seconds
+                flushInterval: 30000
+            },
+            defaultTags: legacyDefaultTags = {},
+            debug = false,
+        } = config;
+
+        const allUserDefinedTags = {...legacyDefaultTags, ...userDefinedDefaultTags};
+
+        const writeOptions: Partial<WriteOptions> = {
+            ...DEFAULT_WriteOptions,
+            ...restUserWriteOptions,
+            defaultTags: allUserDefinedTags
+        }
+
+        if (debug && logger !== undefined) {
+            writeOptions.writeFailed = (error: Error, lines: Array<string>, attempt: number, expires: number): Promise<void> | void => {
+                logger.debug(`Write failed for ${lines.length} lines after ${attempt}`);
+                if(error.message.includes('Request timed out')) {
+                    logger.debug(`Influx Error: ${error.message}`);
+                } else {
+                    logger.debug(error);
+                }
+            };
+            writeOptions.writeSuccess = (lines: Array<string>) => {
+                logger.debug(`Flushed ${lines.length} lines to server`);
+            };
+            writeOptions.writeRetrySkipped = (entry: { lines: Array<string>; expires: number }) => {
+                logger.debug(`Skipped ${entry.lines.length} lines due to full buffer?`);
             }
-        });
+        }
+
+        return writeOptions;
     }
 
     childClient(logger: Logger, tags: Record<string, string> = {}) {
--- src/Common/Influx/interfaces.ts
+++ src/Common/Influx/interfaces.ts
@@ -1,8 +1,11 @@
-import {InfluxDB, WriteApi} from "@influxdata/influxdb-client/dist";
+import {InfluxDB, WriteApi, WriteOptions} from "@influxdata/influxdb-client/dist";
 
 export interface InfluxConfig {
     credentials: InfluxCredentials
     defaultTags?: Record<string, string>
+    writeOptions?: WriteOptions
+    useKeepAliveAgent?: boolean
+    debug?: boolean
 }
 
 export interface InfluxCredentials {
