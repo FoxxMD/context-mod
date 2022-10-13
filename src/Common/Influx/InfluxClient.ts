457f94760340af1379ca25e3fbbb8f8142484fe4
FoxxMD <FoxxMD@users.noreply.github.com>
2022-10-13 12:34:11 -0400

    fix: Further improvements for influxdb logging
    * Suppress write failure warnings to reduce noise in log
      * Can be toggled using debug flag in config

--- src/Common/Influx/InfluxClient.ts
+++ src/Common/Influx/InfluxClient.ts
@@ -1,5 +1,5 @@
 import {InfluxConfig} from "./interfaces";
-import {InfluxDB, Point, WriteApi, setLogger, DEFAULT_WriteOptions, ClientOptions, DEFAULT_RetryDelayStrategyOptions} from "@influxdata/influxdb-client";
+import {InfluxDB, Point, WriteApi, setLogger, DEFAULT_WriteOptions, ClientOptions, DEFAULT_RetryDelayStrategyOptions, Logger as InfluxLogger} from "@influxdata/influxdb-client";
 import {HealthAPI} from "@influxdata/influxdb-client-apis";
 import dayjs, {Dayjs} from "dayjs";
 import {Logger} from "winston";
@@ -11,6 +11,28 @@ import {WriteOptions} from "@influxdata/influxdb-client/dist";
 export interface InfluxClientConfig extends InfluxConfig {
     client?: InfluxDB
     ready?: boolean
+}
+
+/**
+ * Suppress non-error write failures
+ *
+ * These have not yet hit the max retry. On max retry failure Influx logs as ERROR.
+ * The non-error failures are super noisy in the log so suppress them UNLESS debug is turned on
+ *
+ * https://github.com/influxdata/influxdb-client-js/blob/master/packages/core/src/impl/WriteApiImpl.ts#L221
+ * */
+const extendLogger = (logger: Logger, suppressWriteWarnings = true): InfluxLogger => {
+    return {
+        ...logger,
+        error: (message: string, err?: any) => logger.error(message, err),
+        warn: (message: string, err?: any) => {
+            if(suppressWriteWarnings && !message.includes('Write to InfluxDB failed (attempt')) {
+                logger.warn(message, err);
+            } else {
+                logger.warn(message, err);
+            }
+        }
+    }
 }
 
 export class InfluxClient {
@@ -40,7 +62,7 @@ export class InfluxClient {
             this.client = client;
         } else {
             this.client = InfluxClient.createClient(this.config);
-            setLogger(this.logger);
+            setLogger(extendLogger(this.logger, !(rest.debug ?? false)));
         }
 
         this.write = this.client.getWriteApi(config.credentials.org, config.credentials.bucket, 'ms', InfluxClient.createWriteOptions(this.config, this.logger));
@@ -146,19 +168,11 @@ export class InfluxClient {
         }
 
         if (debug && logger !== undefined) {
-            writeOptions.writeFailed = (error: Error, lines: Array<string>, attempt: number, expires: number): Promise<void> | void => {
-                logger.debug(`Write failed for ${lines.length} lines after ${attempt}`);
-                if(error.message.includes('Request timed out')) {
-                    logger.debug(`Influx Error: ${error.message}`);
-                } else {
-                    logger.debug(error);
-                }
-            };
             writeOptions.writeSuccess = (lines: Array<string>) => {
                 logger.debug(`Flushed ${lines.length} lines to server`);
             };
             writeOptions.writeRetrySkipped = (entry: { lines: Array<string>; expires: number }) => {
-                logger.debug(`Skipped ${entry.lines.length} lines due to full buffer?`);
+                logger.warn(`Skipped flushing ${entry.lines.length} lines due to full buffer`);
             }
         }
 
