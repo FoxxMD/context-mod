fde28362087937016613cdc87e2ef06772a2aa3a
FoxxMD <FoxxMD@users.noreply.github.com>
2022-11-28 14:36:33 -0500

    chore: remove comments about wrong endpoints
    At some point maybe this was fixed by reddit silently?

--- src/Action/SubmissionAction/FlairAction.ts
+++ src/Action/SubmissionAction/FlairAction.ts
@@ -43,10 +43,6 @@ export class FlairAction extends Action {
         if (item instanceof Submission) {
             if(!this.dryRun) {
                 if (this.flair_template_id) {
-                    // typings are wrong for this function, flair_template_id should be accepted
-                    // assignFlair uses /api/flair (mod endpoint)
-                    // selectFlair uses /api/selectflair (self endpoint for user to choose their own flair for submission)
-                    // @ts-ignore
                     await item.selectFlair({flair_template_id: this.flair_template_id}).then(() => {});
                     item.link_flair_template_id = this.flair_template_id;
                 } else {
